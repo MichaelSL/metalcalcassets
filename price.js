79065edfa0fcdd6ef7bec50eb5dd2833baf86416
Michael <michael@webbuild.com.ua>
2017-04-04 23:38:54 +0200

    added a sample pricelist

---
+++ price.js
@@ -0,0 +1,143 @@
+{
+    date: "2017-04-04T20:10:48.716Z",
+    prices: [
+        {
+            id: "steel",
+            name: "Сталь",
+            weightPerMeter: 7850,
+            price: 9.38
+        },
+        {
+            id: "aluminium",
+            name: "Алюминий",
+            weightPerMeter: 2600,
+            price: 9.38
+        },
+        {
+            id: "bronse",
+            name: "Бронза",
+            weightPerMeter: 8800,
+            price: 9.38
+        },
+        {
+            id: "avialuminium",
+            name: "Дюралюминий",
+            weightPerMeter: 2800,
+            price: 9.38
+        },
+        {
+            id: "brass",
+            name: "Латунь",
+            weightPerMeter: 8350,
+            price: 9.38
+        },
+        {
+            id: "magnesium",
+            name: "Магний",
+            weightPerMeter: 1740,
+            price: 9.38
+        },
+        {
+            id: "copper",
+            name: "Медь",
+            weightPerMeter: 8930,
+            price: 9.38
+        },
+        {
+            id: "nickel",
+            name: "Никель",
+            weightPerMeter: 8350,
+            price: 9.38
+        },
+        {
+            id: "nichrome",
+            name: "Нихром",
+            weightPerMeter: 8300,
+            price: 9.38
+        },
+        {
+            id: "lead",
+            name: "Олово",
+            weightPerMeter: 7300,
+            price: 9.38
+        },
+        {
+            id: "plumbum",
+            name: "Свинец",
+            weightPerMeter: 11400,
+            price: 9.38
+        },
+        {
+            id: "stainlesssteel",
+            name: "Нержавеющая сталь",
+            weightPerMeter: 7900,
+            price: 9.38
+        },
+        {
+            id: "chrome",
+            name: "Хром",
+            weightPerMeter: 7200,
+            price: 9.38
+        },
+        {
+            id: "zinc",
+            name: "Цинк",
+            weightPerMeter: 7000,
+            price: 9.38
+        },
+        {
+            id: "iron",
+            name: "Чугун",
+            weightPerMeter: 7300,
+            price: 9.38
+        },
+        {
+            id: "titan",
+            name: "Титан",
+            weightPerMeter: 4500,
+            price: 9.38
+        },
+        {
+            id: "silver",
+            name: "Серебро",
+            weightPerMeter: 10500,
+            price: 9.38
+        },
+        {
+            id: "gold",
+            name: "Золото",
+            weightPerMeter: 19320,
+            price: 9.38
+        },
+        {
+            id: "platinum",
+            name: "Платина",
+            weightPerMeter: 21450,
+            price: 9.38
+        },
+        {
+            id: "textolite",
+            name: "Текстолит",
+            weightPerMeter: 1350,
+            price: 9.38
+        },
+        {
+            id: "caprolon",
+            name: "Капролон",
+            weightPerMeter: 1150,
+            price: 9.38
+        },
+        {
+            id: "fluoroplastic",
+            name: "Фторопласт-4",
+            weightPerMeter: 2200,
+            price: 9.38
+        },
+        {
+            id: "paronite",
+            name: "Паронит",
+            weightPerMeter: 1800,
+            price: 9.38
+        }
+    ]
+}
